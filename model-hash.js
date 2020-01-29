d4b442f59320a8af4a39097f40f586d6238dd045
Anudit Nagar <nagaranudit@gmail.com>
2020-01-29 11:09:30 +0530

    Setup Internal Node API

---
+++ model-hash.js
@@ -0,0 +1,217 @@
+let Web3 = require('web3');
+
+const contractABI=[
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_modelHash",
+				"type": "string"
+			},
+			{
+				"name": "_rounds",
+				"type": "uint256"
+			}
+		],
+		"name": "createTask",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_taskID",
+				"type": "uint256"
+			},
+			{
+				"name": "_modelHash",
+				"type": "string"
+			}
+		],
+		"name": "updateModelForTask",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "taskID",
+				"type": "uint256"
+			},
+			{
+				"indexed": true,
+				"name": "_user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "_amt",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "_time",
+				"type": "uint256"
+			}
+		],
+		"name": "newTaskCreated",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "taskID",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "_modelHash",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "_time",
+				"type": "uint256"
+			}
+		],
+		"name": "modelUpdated",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getTaskCount",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_taskID",
+				"type": "uint256"
+			}
+		],
+		"name": "getTaskHashes",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getTasksOfUser",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "SentinelTasks",
+		"outputs": [
+			{
+				"name": "taskID",
+				"type": "uint256"
+			},
+			{
+				"name": "currentRound",
+				"type": "uint256"
+			},
+			{
+				"name": "totalRounds",
+				"type": "uint256"
+			},
+			{
+				"name": "cost",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "UserTaskIDs",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+const contractAddress = "0xdFF74cBcD63811C050A6a2545E62bF7960C55671";
+const provider = "https://testnet2.matic.network";
+
+const web3 = new Web3(provider);
+
+const Sentinel = new web3.eth.Contract(contractABI, contractAddress.toLowerCase());
+
+let argv = process.argv;
+if(argv.length < 3){
+    console.log(-1)
+}
+else {
+    // console.log(argv);
+    Sentinel.methods.getTaskHashes(argv[2]).call(function (error, result){
+        if(!error){
+            console.log(result.toString());
+        } else{
+            console.log(error);
+        }
+    });
+
+}
